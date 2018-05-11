16b13e3cc13fdf522d4082f27970789837b7716b
Pogrebnyak, Sergei <Sergei.Pogrebnyak@softwareag.com>
2018-05-10 17:17:12 -0700

    Fix installation for UM and MSC plus tests

--- templates/sag-msc-server/test.sh
+++ templates/sag-msc-server/test.sh
@@ -5,29 +5,28 @@ if [ -d $SAG_HOME/profiles/SPM ] ; then
     # point to local SPM
     export CC_SERVER=http://localhost:8092/spm
 
-    # TODO: UPDATE ME!!!
-    
-    # echo "Verifying managed container $CC_SERVER ..."
-    # sagcc get inventory products -e TES --wait-for-cc
+    echo "Verifying managed container $CC_SERVER ..."
+    sagcc get inventory products -e MSC --wait-for-cc
 
-    # export CC_WAIT=5
-    # # echo "Verifying fixes ..."
-    # # sagcc get inventory fixes -e wMFix.TES
+    export CC_WAIT=60
+    # echo "Verifying fixes ..."
+    # sagcc get inventory fixes -e wMFix.integrationServer.Core
 
-    # echo "Verifying instances ..."
-    # sagcc get inventory components -e TES-default
+    echo "Verifying instances ..."
+    sagcc get inventory components -e OSGI-IS_msc
 
-    # echo "Start the instance ..."
-    # sagcc exec lifecycle components TES-default start -e DONE --sync-job
+    echo "Start the instance ..."
+    sagcc exec lifecycle components OSGI-IS_msc start -e DONE --sync-job
 
-    # echo "Verifying status ..."
-    # sagcc get monitoring runtimestatus TES-default -e ONLINE -w 60
+    echo "Verifying status ..."
+    sagcc get monitoring runtimestatus OSGI-IS_msc -e ONLINE
+    sagcc get monitoring runtimestatus integrationServer-msc -e ONLINE
 
     # echo "Verifying configs ..."
     # sagcc get configuration data OSGI-SPM COMMON-SYSPROPS -f text -e hello.world
 fi
 
 echo "TODO: Verifying product runtime ..."
-# How?
+curl Administrator:manage -s http://`hostname`:5555/
 
 echo "DONE testing"
