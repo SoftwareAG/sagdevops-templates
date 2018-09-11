c8c43fcadb9ade22e5ce6efdf8d5c6d4f7eb4f95
Rainer Schöneberger <Rainer.Schoeneberger@softwareag.com>
2018-09-11 14:45:12 +0200

    EXX-10797 Build EntireX Broker Docker image with CC Docker Builder

--- templates/sag-exx-broker/entrypoint.sh
+++ templates/sag-exx-broker/entrypoint.sh
@@ -18,7 +18,73 @@
 #
 #*******************************************************************************
 
-$SAG_HOME/register.sh
+#!/bin/bash
 
-# TODO: replace this with actual EXX Broker from $EXXDIR/bin !
-tail -f $SAG_HOME/profiles/SPM/logs/wrapper.log
+# Check if there is a customer specific license file ${EXX_LICENSE_KEY} in the /licenses directory, 
+# if there is one copy the content to ${EXXDIR}/config/license.xml. 
+function overwrite_license {
+echo "Looking for license file ${EXX_LICENSE_KEY} in /licenses."
+if [ -f /licenses/${EXX_LICENSE_KEY} ]; then
+  echo "${EXX_LICENSE_KEY} found copy content to ${EXXDIR}/config/license.xml."
+  cp /licenses/${EXX_LICENSE_KEY} ${EXXDIR}/config/license.xml
+else
+  echo "No ${EXX_LICENSE_KEY} in /licenses found."
+fi
+}
+
+# Check if there is a customer specific attribute file ${EXX_ATTRIBUTE} 
+# in the /configs directory, if there is one copy it to the right location. 
+function overwrite_attribute {
+echo "Looking for attribute file ${EXX_ATTRIBUTE} in /configs."
+if [ -f /configs/${EXX_ATTRIBUTE} ]; then
+  echo "${EXX_ATTRIBUTE} found copy content to ${EXXDIR}/config/etb/${ETBID}/etbfile."
+  cp /configs/${EXX_ATTRIBUTE} ${EXXDIR}/config/etb/${ETBID}/etbfile
+else
+  echo "No ${EXX_ATTRIBUTE} in /configs found, using default."
+fi
+}
+
+# Check if there is a customer specific certificates 
+# in the /configs directory, if there is one copy it to the right location. 
+function overwrite_certificates {
+echo "Looking for ${EXX_KEY_FILE} in /configs."
+if [ -f /configs/${EXX_KEY_FILE} ]; then
+  echo "${EXX_KEY_FILE} found copy it to ${EXXDIR}/config/etb/${ETBID}."
+  cp /configs/${EXX_KEY_FILE} ${EXXDIR}/config/etb/${ETBID}
+else
+  echo "No ${EXX_KEY_FILE} in /configs found, using default."
+fi
+
+echo "Looking for ${EXX_KEY_STORE} in /configs."
+if [ -f /configs/${EXX_KEY_STORE} ]; then
+  echo "${EXX_KEY_STORE} found copy it to ${EXXDIR}/config/etb/${ETBID}."
+  cp /configs/${EXX_KEY_STORE} ${EXXDIR}/config/etb/${ETBID}
+else
+  echo "No ${EXX_KEY_STORE} in /configs found, using default."
+fi
+
+echo "Looking for ${EXX_TRUST_STORE} in /configs."
+if [ -f /configs/${EXX_TRUST_STORE} ]; then
+  echo "${EXX_TRUST_STORE} found copy it to ${EXXDIR}/config/etb/${ETBID}."
+  cp /configs/${EXX_TRUST_STORE} ${EXXDIR}/config/etb/${ETBID}
+else
+  echo "No ${EXX_TRUST_STORE} in /configs found, using default."
+fi
+}
+
+overwrite_license
+overwrite_attribute
+overwrite_certificates
+
+# Setting necessary environment variables
+export PATH="${EXXDIR}/bin:${PATH}"
+export EXXVERS="."
+export LD_LIBRARY_PATH="${EXXDIR}/lib:${LD_LIBRARY_PATH}"
+
+# Starting etbnuc and sending it into the background.
+${EXXDIR}/bin/etbnuc -a ${EXXDIR}/config/etb/${ETBID}/etbfile &
+
+# Wait forever until container shuts down
+wait
+
+exit 0
