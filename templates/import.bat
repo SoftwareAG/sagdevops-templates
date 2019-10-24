4f725c8f11a23e1b0f3fbd1c6c92f5991d16fe8f
Tcholtchev <antc@softwareag.com>
2019-10-24 15:41:23 +0300

    update import script

--- templates/import.bat
+++ templates/import.bat
@@ -5,10 +5,11 @@ IF %ERRORLEVEL% NEQ 0 ECHO Error: sagcc is not installed.
 
 set template=template.yaml
 for /D %%D in (*) do (    
-	sagcc exec templates composite import -i %%D\%template% overwrite=true
+    ECHO "Uploading %%D\%template%"
+    cmd /c sagcc exec templates composite import -i %%D\%template% overwrite=true
 )
 
 ECHO "Register layer definitions"
-sagcc exec templates composite apply sag-cc-layer-defs --wait-for-cc 120 --sync-job -w 3600
+cmd /c sagcc exec templates composite apply sag-cc-layer-defs --wait-for-cc 120 --sync-job -w 3600
 
 
